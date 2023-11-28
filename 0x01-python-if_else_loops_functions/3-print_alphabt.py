00ec16db4bcfcd3770b66a2162943fc76ce6cb00
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-11-28 17:11:16 +0200

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 # 3-print_alphabt.py - prints ASC alphabets except q and e
 for letter in range(97, 123):
     if letter != 101 and letter != 113:
-        print(f"{chr(letter)}", end="")
+        print("{}".format(chr(letter)), end='')
