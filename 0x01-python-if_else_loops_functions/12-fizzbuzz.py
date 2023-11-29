5efa9f51ec8c643dc75ef2c2c53edcc8172c674c
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-11-29 22:09:14 +0200

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,10 +3,10 @@ def fizzbuzz():
     """prints the numbers from 1 to 100"""
     for number in range(1, 101):
         if number % 3 == 0:
-            print("{}".format('Fizz'), end=" ")
+            print("{} ".format('Fizz'), end="")
         elif number % 5 == 0:
-            print("{}".format('Buzz'), end=" ")
+            print("{} ".format('Buzz'), end="")
         elif number % 3 == 0 and number % 5 == 0:
-            print("{}".format('FizzBuzz'), end=" ")
+            print("{} ".format('FizzBuzz'), end="")
         else:
-            print("{}".format(number), end=" ")
+            print("{} ".format(number), end="")
