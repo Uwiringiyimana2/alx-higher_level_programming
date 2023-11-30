a423da22712f286d89a837e214ddb7444500afff
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-11-30 19:42:07 +0200

    5-variable_load.py

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 5-variable_load.py
+
+if __name__ == "__main__":
+    """imports the variable a from variable_load_5.py and prints its value"""
+    import variable_load_5
+
+    print(variable_load_5.a)
