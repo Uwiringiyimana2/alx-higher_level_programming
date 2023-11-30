934887609d192a1b06c5f96c5da1f84f39af630c
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-11-30 17:03:34 +0200

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+# 0_add.py
 
 if __name__ == "__main__":
+    """Print the sum of 1 and 2"""
     import add_0
+
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add_0.add(a, b)))
