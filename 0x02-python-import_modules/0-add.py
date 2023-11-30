713946374fccac3f8d6d3e36ce6ce30b050e2e99
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-11-30 16:54:14 +0200

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import add_0
-
-a = 1
-b = 2
+def main():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
 
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    main()
