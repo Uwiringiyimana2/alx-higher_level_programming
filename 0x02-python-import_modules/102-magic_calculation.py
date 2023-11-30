49a701644ae7f9462a12ce653de9f43293a0ff01
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-11-30 21:05:11 +0200

    102-magic_calculation.py

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 102-magic_calculation.py
+
+def magic_calculation(a, b):
+    c = 0
+
+    if a < b:
+        add = __import__('magic_calculation_102').add
+        sub = __import__('magic_calculation_102').sub
+        c = add(a, b)
+
+        for i in range(4, 6):
+            c = add(c, i)
+
+    else:
+        c = __import__('magic_calculation_102').sub(a, b)
+
+    return c
