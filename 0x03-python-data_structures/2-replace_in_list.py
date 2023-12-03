0d367cc58041b782145a66f7ac1021ac39cfe2b0
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-12-03 09:02:08 +0200

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -13,8 +13,6 @@ def replace_in_list(my_list, idx, element):
         modified list
         orginal list if idx is negative or out of range
     """
-    if idx < 0 or idx > (len(my_list)):
-        return my_list
-    else:
+    if idx >= 0 or idx <= (len(my_list)):
         my_list[idx] = element
-        return my_list
+    return my_list
