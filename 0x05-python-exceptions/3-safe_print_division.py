bf63104d2e5ca20acdae0ec866fa4ce7f0d9a8cb
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-12-18 20:15:11 +0200

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,8 @@ def safe_print_division(a, b):
     result = 0
     try:
         result = a / b
-        print("Inside result: {}".format(result))
-        return (result)
     except ZeroDivisionError:
-        print("Inside result: {}".format(None))
-        return (None)
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return (result)
