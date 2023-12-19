1a3e99c9e7da35d217e7dedc510f9707040afdfb
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-12-19 19:22:47 +0200

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""class Square defines square"""
+
+
+class Square:
+    """class square with private instance attributes: size"""
+
+    def __init__(self, size=0):
+        """Initialize the square instance
+
+        Args:
+            size (int, optional): The size of the square
+
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """public instance method of class square
+
+        Return:
+            int: the current square area
+        """
+        return self.__size ** 2
