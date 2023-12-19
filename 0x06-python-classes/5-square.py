15af29401b6d3c77d4e7b94f6e96cc3c8db4571b
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-12-19 20:33:58 +0200

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""class Square defines square"""
+
+
+class Square:
+    """class square with private instance attributes: size"""
+
+    def __init__(self, size=0):
+        """Initialize the square instance
+
+        Args:
+            size (int, optional): The size of the square
+
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter
+
+        Args:
+            value(int): the size to set
+
+         Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """public instance method of class square
+
+        Return:
+            int: the current square area
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """prints in stdout the square with the character #."""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
