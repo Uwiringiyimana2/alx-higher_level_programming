4da632fc91616ff727f917fa6f53e638ec7f6a8e
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-12-19 20:06:42 +0200

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""class Square defines square"""
+
+
+class Square:
+    """class square with private instance attributes: size"""
+
+    def __init__(self, size=0):
+        """Initialize the square instance
+
+        Args:
+            size (int, optional): The size of the square
+
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+
+    def area(self):
+        """public instance method of class square
+
+        Return:
+            int: the current square area
+        """
+        return self.__size ** 2
