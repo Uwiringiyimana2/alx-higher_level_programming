52b0f6956955068d195cb6509297f1211220d1ff
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2024-01-04 19:21:04 +0200

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -49,4 +49,6 @@ class Rectangle:
 
     def perimeter(self):
         """ returns the rectangle perimeter """
+        if self.width == 0 or self.height == 0:
+            return 0
         return (2 * self.width) + (2 * self.height)
