b928558acb492558d51f757deed6611786066683
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2024-01-08 21:07:20 +0200

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module of class BaseGeometry"""
+
+
+class BaseGeometry:
+    """ base_geometry """
+
+    def area(self):
+        """raises an Exception with the message area() is not implemented"""
+        raise Exception("area() is not implemented")
