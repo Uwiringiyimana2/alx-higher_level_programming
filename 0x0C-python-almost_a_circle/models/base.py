7bebda4f8250eb80bf863eef53ec79ba31241619
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2024-01-15 22:33:56 +0200

    100-main.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """This module defines class Base."""
 
 import json
+import csv
 
 
 class Base:
@@ -52,6 +53,8 @@ class Base:
             instance = cls(1, 1, 0, 0)
         if cls.__name__ == "Square":
             instance = cls(1, 0, 0)
+        else:
+            instance = cls()
         instance.update(**dictionary)
         return instance
 
@@ -67,3 +70,39 @@ class Base:
         except Exception as err:
             pass
         return results
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Serializes and writes to CSV file"""
+        if list_objs is None:
+            list_objs = []
+        filename = cls.__name__ + ".csv"
+        with open(filename, 'w', newline='') as f:
+            writer = csv.writer(f)
+            for instance in list_objs:
+                writer.writerow(instance.to_csv_row())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Deserializes and returns a list of instances fro CSV file."""
+        filename = cls.__name__ + ".csv"
+        results = []
+        try:
+            with open(filename, 'r') as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    results.append(cls.create_from_csv_row(row))
+        except FileNotFoundError:
+            pass
+        return results
+
+    def to_csv_row(self):
+        """Returns a CSV row"""
+        raise NotImplementedError("to_csv_row method must be implemented")
+
+    @classmethod
+    def create_from_csv_row(cls, csv_row):
+        """Creates an instance from a CSV row."""
+        raise NotImplementedError("create_from_csv_row method must be implemented")
+
+
