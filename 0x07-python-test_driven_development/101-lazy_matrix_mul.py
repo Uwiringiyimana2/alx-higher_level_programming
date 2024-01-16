32ff6df413d0cf5310808c648e327e9ab0088bee
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2024-01-16 21:50:21 +0200

    101-lazy_matrix_mul.py

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -6,8 +6,4 @@ import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
     """multiplies 2 matrices by using the module NumPy"""
-    try:
-        result = np.matmul(m_a, m_b)
-        return result.tolist()
-    except ValueError as e:
-        raise ValueError("Error in matrix multiplication: {}".format(e))
+    return np.dot(m_a, m_b)
