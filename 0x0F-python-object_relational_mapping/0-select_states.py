63f6d1b0e3b86d0aa8d5e40691143d3e70562cc9
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2024-03-17 14:50:55 +0200

    ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
+import sys
 
 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root",  passwd="root", db="hbtn_0e_0_usa")
-cur = conn.cursor()
-cur.execute(SELECT * FROM states ORDERBY ASC states.id)
-query_rows = cur.fetchall()
-for row in query_rows:
-    print("{}, {}".format(states.id, states))
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
 
-cur.close()
-conn.close()
+    cur.close()
+    conn.close()
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""akes in the name of a state as an argument and lists all cities 
+    of that state, using the database hbtn_0e_4_usa    
+"""
+import MySQLdb
+import sys
+
+
+if __name__ = "__main__":
+    connaction = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                                passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = connection.cursor()
+    match = sys.argv[4]
+    cur.execute("""SELECT cities.name FROM cities 
+                INNER JOIN states ON states.id=cities.states_id 
+                WHERE states.name=%s""", (match,))
+    rows = cur.fetchall()
+    temp = list(row[0] for row in rows)
+    print(*temp, sep=", ")
+
+    cur.close()
+    connection.close()
