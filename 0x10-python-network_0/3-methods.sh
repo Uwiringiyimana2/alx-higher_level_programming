131966d402b9f434250606d1d9b41ed23554a0c9
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2024-03-31 13:22:58 -0700

    Networking0

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # sends a request to that URL, and displays the size of the body of the response 
 curl -sI $1 | grep "Allow" | cut -d " " -f 2-
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # sends a request to that URL, and displays the size of the body of the response 
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
