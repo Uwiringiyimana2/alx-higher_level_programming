72911eefaeb2988861ee0d4dc744aef6fa96c57e
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2024-04-01 12:34:00 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+""" fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
         html = response.read()
 
         print("Body response:")
