74edcefa2cfddc1d4d86e0355450b0f3c9c8c0a8
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2024-04-24 14:40:01 +0200

    js api

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,3 +1,5 @@
+#!/usr/bin/node
+
 const request = require('request');
 const url = process.argv[2];
 
@@ -10,11 +12,11 @@ request(url, function (err, response, body) {
     for (const i in tasks) {
       const task = tasks[i];
       if (task.completed === true) {
-	if (completed[task.userId] === undefined) {
+        if (completed[task.userId] === undefined) {
           completed[task.userId] = 1;
-	} else {
-	  completed[task.userId]++;
-	}
+        } else {
+          completed[task.userId]++;
+        }
       }
     }
     console.log(completed);
